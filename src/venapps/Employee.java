a579d6234da9f4fd551a968c9a2f5396c3297485
Praveen <Praveen@192.168.0.2>
2017-08-31 15:55:45 +0530

    Sring and method addded

--- src/venapps/Employee.java
+++ src/venapps/Employee.java
@@ -4,8 +4,13 @@ public class Employee
 {
 	int empId=10;
 	String empName = "Praveen";
+	String empName1 = "Dude";
 	
 	public void call()
+	{
+		System.out.println(" Hello ");
+	}
+	public void call1()
 	{
 		System.out.println(" Hello ");
 	}
